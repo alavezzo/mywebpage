3bad50794ac3dd5fb2f28bdc6298eef516973d10
Anthony Lavezzo <anthonylavezzo@Anthonys-MBP.lan>
2023-11-08 00:28:49 -0800

    create react app, initial color and font decisions. beginning design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
 import logo from './logo.svg';
 import './App.css';
+import "./fonts/VaguelyFatal.ttf"
 
 function App() {
   return (
     <div className="App">
+      <div className='left-column'>
+        `<div className='sub-header'>
+          <h2  style={{textAlign: 'right'}}>Artist and Engineer</h2>
+          <p className='second-p'>
+            create art
+          </p>
+        </div>
+        <div className='sub-header'>
+            <h2 style={{textAlign: 'left'}}>My </h2>
+            <p style={{textAlign: 'left'}}>
+            Make music
+            </p>
+          </div>
+        </div>`
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1>
+          A
+        </h1>
+        <h1>
+          n
+        </h1>
+        <h1>
+          t
+        </h1>
+        <h1>
+          h
+        </h1>
+        <h1>
+         o
+        </h1>
+        <h1>
+          n
+        </h1>
+        <h1>
+          y 
+        </h1>
+        <h1>
+          L
+        </h1>
+        <h1>
+        a
+        </h1>
+        <h1>
+          v
+        </h1>
+        <h1>
+         e
+        </h1>
+        <h1>
+         z
+        </h1>
+        <h1>
+         z 
+        </h1>
+        <h1>
+        o 
+        </h1>
+        <p>FullStack Developer </p>
       </header>
+      <div className='right-column'>
+        <div className='sub-header'>
+          <h2 style={{textAlign: 'right'}}>Artist and Engineer</h2>
+          <p>
+            I build websites, mobile applications
+          </p>
+        </div>
+
+        <div className='sub-header'>
+          <h2 style={{textAlign: 'right'}}></h2>
+          <p style={{textAlign: 'left'}}>
+           Make music
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
